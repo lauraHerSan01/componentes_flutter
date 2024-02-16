74d3789d69f889386ab2f3975543494cf4c37f3b
laura <hernandez.sandoval.laura@gmail.com>
2024-02-16 10:35:41 -0600

    Adding a simple ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,54 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Componentes de Flutter'),
+      ),
+      body: ListView(
+        children: const [
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+          Text('KC campeones del Super Bowl 58'),
+        ],
+      ),
+    );
   }
 }

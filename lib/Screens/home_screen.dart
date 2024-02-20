9091ab28ae394d91e423324b366c24655491ce0f
laura <hernandez.sandoval.laura@gmail.com>
2024-02-20 12:00:18 -0600

    Adding GoogleFonst and Textheme

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,26 +10,36 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Componentes de Flutter'),
       ),
       body: ListView(
-        children:const <Widget> [
+        children: <Widget> [
           ListTile(
-            title: Text('El super Bowl'),
-            subtitle: Text('Bon Sponja - Sweet Victory'),
-            leading: Icon(Icons.casino),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+            title: Text(
+              'Entradas',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+              ),
+            subtitle: Text('Introducioendo valores',
+             style: AppTheme.lightTheme.textTheme.bodySmall,
+            ),
+            leading: const Icon(Icons.input),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
-          Divider(),
-          ListTile(
-            title: Text('El super Bowl'),
-            subtitle: Text('Alicia Keys - If I aint Got you y my boo'),
-            leading: Icon(Icons.beach_access),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+          const Divider(),
+           ListTile(
+            title: Text('Listview.builder',
+             style: AppTheme.lightTheme.textTheme.headlineLarge,
+             ),
+            subtitle: const Text('Lista con sroll infinito'),
+            leading: const Icon(Icons.beach_access),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
-          Divider(),
-          ListTile(
-            title: Text('El super Bowl'),
-            subtitle: Text('Post Malone - America The Beautiful'),
-            leading: Icon(Icons.safety_check),
-            trailing: Icon(Icons.arrow_circle_right_rounded),
+          const Divider(),
+           ListTile(
+            title: Text(
+              'Notificaciones',
+             style: AppTheme.lightTheme.textTheme.headlineLarge,
+             ),
+            subtitle: const Text('Mostar una notificacion '),
+            leading: const Icon(Icons.input),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
           ),
         ],
       ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,18 +1,39 @@
 //Definicion de los temas de estilo de la app 
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 class AppTheme{
 //Constantes de color primario
-  static const primatyColoo = Color.fromARGB(255, 255, 0, 0);
+  static const primatyColor = Color.fromARGB(255, 5, 164, 98);
+  //constante de color secundario
+  static const secondaryColor = Color.fromARGB(255, 4, 71, 196);
+  //cponstante de color de fondo 
+  static const backColor = Color.fromARGB(255, 27, 28, 28);
 
-  static const backColor = Color.fromARGB(45, 3, 67, 65);
 //Constante de tema
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      scaffoldBackgroundColor: const Color.fromARGB(142, 128, 100, 156),
-      primaryColor: const Color.fromARGB(246, 217, 12, 115),
+      scaffoldBackgroundColor: const Color.fromARGB(142, 175, 174, 175),
+      primaryColor: const Color.fromARGB(246, 247, 247, 247),
       appBarTheme: const AppBarTheme(
-        color: Color.fromARGB(255, 234, 145, 789),
-  ));
+        color: Color.fromARGB(255, 97, 238, 191)
+  ),
+  textTheme: TextTheme(
+    //titulos muy grandes 
+    headlineLarge: GoogleFonts.prozaLibre(
+    color: primatyColor,
+    fontSize: 26.5,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.italic,
+    decoration:TextDecoration.underline,
+    decorationColor: primatyColor,
+    decorationStyle: TextDecorationStyle.dotted,
+  ),
+  //Estilo para texto muy pequeño
+    bodySmall: GoogleFonts.abel(
+      color: secondaryColor,
+      fontSize: 16.0,
+    ),
+  ),
+  );
   
-  static final ThemeData darkTheme = ThemeData.dark().copyWith();
 }
+

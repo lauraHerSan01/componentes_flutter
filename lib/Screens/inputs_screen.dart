b9c023490f7024ebd75d366bd45c41981b8ff989
laura <hernandez.sandoval.laura@gmail.com>
2024-03-04 13:33:32 -0600

    Adding BottomNavigationBar

--- lib/Screens/inputs_screen.dart
+++ lib/Screens/inputs_screen.dart
@@ -17,33 +17,35 @@ class _InputsScreenStateState extends State<InputsScreenState> {
       appBar: AppBar(
         title: const Text('Entradas'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Entradas',
-            style: AppTheme.lightTheme.textTheme.headlineLarge,
-          ),
-          entradaNombre(),
-          entradaSwitch(),
-          entradaSlider(),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: null,
-                child: Text('Regreasar',
-                )
-              ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('Ventana de Datos',
-                )  
-              ),
-            ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            entradaNombre(),
+            entradaSwitch(),
+            entradaSlider(),
+            const ElevatedButton(
+                  onPressed: null,
+                  child: Text(
+                    'Guardar',
+                  )),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppTheme.primatyColor,
+        items:const  [
+          BottomNavigationBarItem(
+            icon:   Icon(Icons.home),
+            label: 'Inicio'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.data_exploration),
+            label: 'Datos',
           ),
         ],
       ),
+      
     );
   }
 

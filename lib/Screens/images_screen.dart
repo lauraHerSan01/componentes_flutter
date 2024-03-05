58dd0ff12f24915f8f9078433c000956a9a758a8
laura <hernandez.sandoval.laura@gmail.com>
2024-03-05 12:03:29 -0600

    Imagenes

--- lib/Screens/images_screen.dart
+++ lib/Screens/images_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -14,14 +16,60 @@ class _ImageScreenState extends State<ImageScreen> {
       appBar: AppBar( 
       title: const Text('Imagenes'),
       ),
-      body:const Column(
-        children: [
-           Image(
-            image: AssetImage('assets/IMG/feria.jpg'),
-          )
-
+      body: ListView(
+        children: [cardImage1(),
+        image2(),
         ],
       ),
+    );
+  }
+
+  Card cardImage1(){
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30), 
+      ),
+      margin: const EdgeInsets.all(15),
+      elevation: 10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Column(
+        children: [
+          const Image(
+            image: AssetImage('assets/IMG/feria.jpg'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'Una Feria',
+              style: AppTheme.lightTheme.textTheme.headlineMedium,
+            ),
+          )
+        ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox image2(){
+    return SizedBox(
+      height: 100.0,
+      width: 100.0,
+      child: Image.network('https://bombanoise.com/wp-content/uploads/2019/04/mocking-spongebob-meme-1.jpg'),
+      );
+  }
+  Stack imageFade(){
+    return Stack(
+      children: <Widget> [
+       const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSz1vlN_bEvfVHx7yoIrsHVpbLbPEHZC8ueny1Xb3aDRH6t_UlF'),
+        ),
+        ],
     );
   }
 }
